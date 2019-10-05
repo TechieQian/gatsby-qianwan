7ef8e0552b196b874790b8b201b8bba46714d8d3
qian wan <qiansan@gmail.com>
2019-10-05 10:49:25 -0400

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,9 +13,17 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+      <SomeComponent message="world" />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+interface ComponentProps {
+  message: string
+}
+const SomeComponent = (props: ComponentProps) => {
+  return <div> hello {props.message} </div>
+}
 
 export default IndexPage
