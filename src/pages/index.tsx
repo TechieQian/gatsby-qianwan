8188ddac551ea9ad698584347659fda23cfdb14d
qwan <qiansan@gmail.com>
2021-10-22 07:56:29 -0400

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,16 @@ import "bulma/css/bulma.css"
 
 const IndexPage = () => (
   <article>
-    <b>Hello!</b> My name is Qian and I am a software engineer with 10 years
+    <b>Hello!</b> My name is Qian and I am a software engineer with over 10 years
     experience in fintech. I began creating webpages during the geocities days
-    and had the pleasure/pain of coding through the webdev eras. In college I
+    and had the pain and pleasure of coding through the eras. In college I
     majored in computer engineering and obsessed over FPGA and C++. But I
-    eventually found my way back home in webdev. My comfort is ASP.NET and
-    React/TypeScript.
+    eventually found my way back home in webdev and lean towards Front-End. 
+    I enjoy interface-driven architecture whether it be a React component or 
+    a protobuff file. Simplicity is complicated. 
+
+    Talk to me about React, browser engines, and reasons we don't have gRPC
+    streaming over web yet. 
   </article>
 )
 
